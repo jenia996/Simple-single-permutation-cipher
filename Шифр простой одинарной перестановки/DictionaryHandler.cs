5c58191155eb6116ed5f1108caf77ab8c00f9859
jenia996 <jenian996@gmail.com>
2015-07-09 12:04:42 +0300

    Program can encode and decode text with simple single permutation cipher

---
+++ Шифр простой одинарной перестановки/DictionaryHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Шифр_простой_одинарной_перестановки
+{
+    class DictionaryHandler
+    {
+        private String path = @"r:\University\Вычислительная Практика\Dictionary\Dictionary.txt";
+        private List<String> dictionary;
+        public DictionaryHandler ()
+        {
+            dictionary = new List<string>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+
+                while (reader.EndOfStream != true)
+                {
+                    dictionary.Add(reader.ReadLine());
+                }
+            }
+        }
+        private List<char> getSymbols(String input)
+        {
+            List<Char> symbols = new List<char>();
+            foreach(Char x in input)
+            {
+                symbols.Add(x);
+            }
+            return symbols;
+        }
+        private Boolean checkWord(String word,List<Char> symbols)
+        {
+            List<Char> matchWordSymbols = word.ToList();
+            if(Enumerable.SequenceEqual(matchWordSymbols.OrderBy(x =>x),symbols.OrderBy(t =>t)))
+            {
+                return true;
+            }
+            return false;
+        }
+        public List<String> findAnswers(String input)
+        {
+          // List<String>.Enumerator iterator = matches.GetEnumerator();
+          // iterator.MoveNext();
+            List<Char> symbols = getSymbols(input.ToLower());
+            List<String> matches = dictionary.FindAll(x => x.Length == input.Length).ToList();
+            List<String> answers = new List<string>();
+            foreach(String word in matches)
+            {
+                if(checkWord(word,symbols))
+                {
+                    answers.Add(word);
+                }
+            }
+            return answers;
+
+        }
+
+    }
+}
---
+++ Шифр простой одинарной перестановки/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Шифр_простой_одинарной_перестановки
+{
+    class Program
+    {
+        private static string read ()
+        {
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                return reader.ReadLine();
+            }
+        }
+        private static List<int> createKey (String input)
+        {
+            Random random = new Random();
+            List<int> key = new List<int>();
+            while (key.Count < input.Length)
+            {
+                int temp = random.Next() % input.Length;
+                if (!key.Contains(temp))
+                {
+                    key.Add(temp);
+                }
+            }
+            return key;
+        }
+        private static String encode (String input, List<int> key)
+        {
+            StringBuilder encodedText = new StringBuilder();
+            foreach (int position in key)
+            {
+                encodedText.Append(input[position]);
+            }
+            return encodedText.ToString();
+        }
+        private static List<int> findSequence (String encodedText, String match)
+        {
+            int j = 0;
+            List<Char> encodedTextSequence = encodedText.ToList();
+            List<Char> matchTextSequence = match.ToList();
+            List<int> sequence = new List<int>();
+            while (sequence.Count < matchTextSequence.Count && j < encodedTextSequence.Count)
+            {
+                List<int> positions = matchTextSequence.Select((c, i) => new { character = c, index = i })
+                         .Where(list => String.Compare(list.character.ToString(), encodedTextSequence[j].ToString(), true) == 0)
+                         .Select(o => o.index).ToList();
+                foreach (int pos in positions)
+                {
+                    if (!sequence.Contains(pos))
+                    {
+                        sequence.Add(pos);
+                        break;
+                    }
+                }
+                j++;
+
+            }
+            return sequence;
+
+        }
+        private static Dictionary<String, List<int>> answersWithKey (String encodedText, List<String> matches)
+        {
+            Dictionary<String, List<int>> answers = new Dictionary<string, List<int>>();
+            foreach (String match in matches)
+            {
+                answers.Add(match, findSequence(encodedText, match));
+            }
+            return answers;
+        }
+        static void Main (string[] args)
+        {
+            String input = read();
+            List<int> key = createKey(input);
+            String encodedText = encode(input, key);
+            DictionaryHandler dicHandler = new DictionaryHandler();
+            List<String> answers = dicHandler.findAnswers(encodedText);
+            using(StreamWriter writer = new StreamWriter("output.txt"))
+            {
+                writer.WriteLine(encodedText);
+                foreach(KeyValuePair<String,List<int>> pair in   answersWithKey(encodedText,answers))
+                {
+                    writer.Write(pair.Key+ " ");
+                    foreach(int x in pair.Value)
+                    {
+                        writer.Write(x+1);
+                        if (key.Count > 10)
+                            writer.Write(" ");
+                    }
+                    writer.WriteLine();
+                }
+                foreach (int x in key)
+                {
+                    writer.Write(x+1);
+                    if (key.Count > 10)
+                        writer.Write(" ");
+                }
+            }
+          
+        }
+    }
+}
